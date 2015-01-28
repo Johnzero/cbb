d300fe1360541d557e0732ed3b4f895f78d880b6
zero <wangsong1233276@126.com>
2015-01-28 17:17:25 +0800

    17.17

---
+++ data/conf/config.php
@@ -0,0 +1,9 @@
+<?php	return array (
+  'SP_DEFAULT_THEME' => 'default',
+  'DEFAULT_THEME' => 'admin',
+  'SP_ADMIN_STYLE' => 'bluesky',
+  'URL_MODEL' => '2',
+  'URL_HTML_SUFFIX' => '.html',
+  'UCENTER_ENABLED' => 0,
+  'COMMENT_NEED_CHECK' => 0,
+);?>
---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'cultural',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cultural_',
+    //密钥
+    "AUTHCODE" => 't464NTkQnhil1B3Xmn',
+    //cookies
+    "COOKIE_PREFIX" => 'ykfkBz_',
+);
+?>
